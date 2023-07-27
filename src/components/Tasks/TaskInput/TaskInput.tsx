b2b4ae1c0574cdf51aa9f68ca34257c247a35129
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-07-27 21:07:47 +0300

    add validation styles

--- src/components/Tasks/TaskInput/TaskInput.tsx
+++ src/components/Tasks/TaskInput/TaskInput.tsx
@@ -10,21 +10,29 @@ type Props = {
 
 const TaskInput: FC<Props> = ({ onAddTask }) => {
   const [inputText, setInputText] = useState<string>('')
+  const [isInputValid, setIsInputValid] = useState<boolean>(true)
 
   const handleTaskInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if(event.target.value.trim()) {
+      setIsInputValid(true)
+    }
     setInputText(event.target.value)
   }
 
   const handleFormSubmit = (event: FormEvent): void => {
     event.preventDefault()
+    if(!inputText.trim()) {
+      setIsInputValid(!isInputValid)
+      return
+    }
     onAddTask(inputText)
   }
 
   return (
     <form onSubmit={handleFormSubmit}>
       <div className="form-control">
-        <label>Задачи</label>
-        <input type="text" onChange={handleTaskInputChange} />
+        <label style={{color: !isInputValid ? 'red' : 'black'}}>Задачи</label>
+        <input type="text" onChange={handleTaskInputChange} style={{borderColor: !isInputValid ? 'red' : 'black', backgroundColor: !isInputValid ? 'salmon' : 'transparent'}} />
       </div>
       <Button buttonType={'submit'}>Добавить Задачу</Button>
     </form>
